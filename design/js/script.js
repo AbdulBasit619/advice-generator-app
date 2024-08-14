7664cfad2c8a6597dd4e84046a2fb3657229dc07
Abdul Basit Dev <619abh@gmail.com>
2024-08-14 11:19:25 +0500

    Finalized

--- design/js/script.js
+++ design/js/script.js
@@ -1,7 +1,7 @@
 ////// Activating Strict Mode
 'use strict';
 
-////////////////////////////////
+///////////////////////////////////
 ///// SELECTING ELEMENTS
 
 const adviceNumber = document.querySelector('.heading--senary');
@@ -11,39 +11,30 @@ const btnDice = document.querySelector('.dice');
 
 const divider = document.querySelector('.divider');
 
-////////////////////////////////
-///// IMPlEMENTING FUNCTIONALITY
+///////////////////////////////////
+///// IMPLEMENTING FUNCTIONALITY
+
+function implementDivider() {
+  const vw = Math.max(
+    this.document.documentElement.clientWidth || 0,
+    this.window.innerWidth || 0
+  );
+  if (vw < 600) {
+    divider.setAttribute('src', './images/pattern-divider-mobile.svg');
+  } else {
+    divider.setAttribute('src', './images/pattern-divider-desktop.svg');
+  }
+}
 
 // Changing divider image according to viewport width
-window.addEventListener('load', function () {
-  const vw = Math.max(
-    document.documentElement.clientWidth || 0,
-    window.innerWidth || 0
-  );
-  if (vw < 600) {
-    divider.setAttribute('src', './images/pattern-divider-mobile.svg');
-  } else {
-    divider.setAttribute('src', './images/pattern-divider-desktop.svg');
-  }
-});
+window.addEventListener('load', implementDivider);
+window.addEventListener('resize', implementDivider);
 
-window.addEventListener('resize', function () {
-  const vw = Math.max(
-    document.documentElement.clientWidth || 0,
-    window.innerWidth || 0
-  );
-  if (vw < 600) {
-    divider.setAttribute('src', './images/pattern-divider-mobile.svg');
-  } else {
-    divider.setAttribute('src', './images/pattern-divider-desktop.svg');
-  }
-});
+// Loading and displaying advice from advice API
 
-// Loading and displaying Advice from advice API
+// Remember that AdviceSlip API documentation says that 'Advice is cached for 2 seconds. Any repeat request within 2 seconds will return the same piece of advice.' So for a different advice, try reloading the page or clicking the dice button after 2 seconds.
 
-// Remember that AdviceSlip API documentation says that 'Advice is cached for 2 seconds. Any repeat-request within 2 seconds will return the same piece of advice.'. So for a different advice, try reloading the page or clicking the dice button after 2 seconds.
-
-const URL = `https://api.adviceslip.com/advice`;
+const URL = 'https://api.adviceslip.com/advice';
 
 function showAdvice(id, advice) {
   adviceNumber.textContent = adviceNumber.textContent
@@ -54,9 +45,9 @@ function showAdvice(id, advice) {
 
   adviceText.textContent = [
     adviceText.textContent.charAt(0),
-    adviceText.textContent.slice(1, -1).replace(/^.*$/, advice),
+    adviceText.textContent.slice(1, -1).replace(/^.*$/, advice), // /^.*$/ is a regular expression that checks for everything
     adviceText.textContent.charAt(adviceText.textContent.length - 1),
-  ].join('');
+  ].join(' ');
 }
 
 async function getAdvice() {
@@ -67,7 +58,7 @@ async function getAdvice() {
 
     const data = await res.json();
 
-    const { id, advice } = data.slip;
+    const { id, advice } = data.slip; // Equivalent to const id = data.slip.id and const advice = data.slip.advice (this method is called destructuring)
 
     showAdvice(id, advice);
   } catch (err) {
@@ -75,6 +66,6 @@ async function getAdvice() {
   }
 }
 
-window.addEventListener('load', getAdvice);
-btnDice.addEventListener('click', getAdvice);
-// btnDice.addEventListener('touchStart', getAdvice);
+window.addEventListener('load', getAdvice); // Get advice whenever window loads
+btnDice.addEventListener('click', getAdvice); // Get advice when btnDice is clicked
+btnDice.addEventListener('touchstart', getAdvice); // Get advice when btnDice is tapped on a touchscreen
